4c3d4542e5bb79ac0f6d15a14c715c8e71318bb8
Veer Pareek <veer@Veers-MBP.attlocal.net>
2024-08-09 18:26:22 -0700

    Process images done

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -6,6 +6,27 @@ import torch
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5] # LLM predicted different values here, it was [0.485, 0.456, 0.406]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
+
+
+def process_images(images: List[Image.Image], size: Dict[str, int] = None, resample: Image.Resampling = None, rescale_factor: float = None, 
+                    image_mean: Optional[Union[float, List[float]]] = None, image_std: Optional[Union[float, List[float]]] = None) -> List[np.ndarray]:
+    height, width = size[0], size[1]
+    images = [resize(image = image, size = (height, width), resample = resample) for image in images]
+    
+    # Convert each image to a numpy array
+    images = [np.array(image) for image in images]
+    # Rescale the pixel values to be in the range [0, 1]
+    images = [rescale(image, scale = rescale_factor) for image in images]
+    # Normalize the images to have mean 0 and standard deviation 1
+    images = [normalize(image, mean = image_mean, std = image_std) for image in images]
+    # Move the channel dimension to the first dimension. The model expects images in format [Channel, Height, Width]
+    images = [image.transpose(2, 0, 1) for image in images]
+    return images
+
+
+
+
+
 class PaliGemmaProcessor:
 
     IMAGE_TOKEN = "<image>"
