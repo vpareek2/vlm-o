3a987f0136784f1f55dbb6686f2145440227b4a2
Veer Pareek <veer@Veers-MBP.attlocal.net>
2024-08-09 19:07:57 -0700

    Init Gemma

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -6,6 +6,7 @@ import math
 from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
 
 class PaliGemmaForConditionalGeneration(nn.Module):
+
     def __init__(self, config: PaliGemmaConfig):
         super().__init__()
         self.config = config
@@ -16,3 +17,32 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         language_model = GemmaForCasualLM(config.text_config)
         self.language_model = language_model
         self.pad_token_id = self.config.pad_token_id if self.config.pad_token_id is not None else -1
+    
+    def tie_weights(self):
+        return self.language_model.tie_weights()
+    
+    def forward(self, input_ids: torch.LongTensor = None, pixel_values: torch.FloatTensor = None, attention_mask: Optional[torch.Tensor] = None,
+                kv_cache: Optional[KVCache] = None) -> Tuple:
+        assert torch.all(attention_mask == 1), "The input cannot be padded"
+
+        # 1. Extra the input embeddings
+        # shape : (Batch_Size, Seq_Len, Hidden_Size)
+        inputs_embeds = self.language_model.get_input_embeddings()(input_ids)
+
+        # 2. Merge text and images
+        # [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+        selected_image_feature = self.vision_tower(pixel_values.to(inputs_embeds.dtype))
+        # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Hidden_Size]
+        image_features = self.multi_modal_projector(selected_image_feature)
+
+        # Merge the embeddings of the text tokens and the image tokens 
+        inputs_embeds, attention_mask, position_ids = self._merge_input_ids_with_image_features(image_features, inputs_embeds, input_ids, attention_mask, kv_cache)
+
+        outputs = self.language_model(
+            attention_mask = attention_mask,
+            position_ids = position_ids,
+            inputs_embeds = inputs_embeds,
+            kv_cache = kv_cache
+        )
+
+        return outputs
