a992e87b935d5a07030f5490ef6e5c89fd2f1aa8
Veer Pareek <veer@Veers-MBP.attlocal.net>
2024-08-09 18:36:00 -0700

    Finished processing file

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -6,6 +6,15 @@ import torch
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5] # LLM predicted different values here, it was [0.485, 0.456, 0.406]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
+def add_image_tokens_to_prompt(prefix_prompt, bos_token, image_seq_len, image_token):
+    # Quoting from the blog (https://huggingface.co/blog/paligemma#detailed-inference-process):
+    #   The input text is tokenized normally.
+    #   A <bos> token is added at the beginning, and an additional newline token (\n) is appended.
+    #   This newline token is an essential part of the input prompt the model was trained with, so adding it explicitly ensures it's always there.
+    #   The tokenized text is also prefixed with a fixed number of <image> tokens.
+    # NOTE: from the paper it looks like the `\n` should be tokenized separately, but in the HF implementation this is not done.
+    #       ref to HF implementation: https://github.com/huggingface/transformers/blob/7f79a97399bb52aad8460e1da2f36577d5dccfed/src/transformers/models/paligemma/processing_paligemma.py#L55-L73
+    return f"{image_token * image_seq_len}{bos_token}{prefix_prompt}\n"
 
 def rescale(image: np.ndarray, scale: float, dtype: np.dtype = np.float32) -> np.ndarray:
     rescaled_image = image * scale
