73937044e51a5b21a2f1a8de5d1166f342228caa
Veer Pareek <veer@Veers-MBP.attlocal.net>
2024-08-09 18:51:33 -0700

    Init Gemma

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -4,3 +4,15 @@ from typing import Optional, Tuple, List
 from torch.nn import CrossEntropyLoss
 import math
 from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
+
+class PaliGemmaForConditionalGeneration(nn.Module):
+    def __init__(self, config: PaliGemmaConfig):
+        super().__init__()
+        self.config = config
+        self.vision_tower = SiglipVisionModel(config.vision_config)
+        self.multi_modal_projector PaliGemmaMultiModalProjector(config)
+        self.vocab_size = config.vocab_size
+
+        language_model = GemmaForCasualLM(config.text_config)
+        self.language_model = language_model
+        self.pad_token_id = self.config.pad_token_id if self.config.pad_token_id is not None else -1
