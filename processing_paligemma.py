2d2f6f7fc79fcc16d42cc6393c7895e9b8b2e5f7
Veer Pareek <veer@Veers-MBP.attlocal.net>
2024-08-09 18:29:24 -0700

    Some helpers added

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -7,6 +7,22 @@ IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5] # LLM predicted different values here, 
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
 
+def rescale(image: np.ndarray, scale: float, dtype: np.dtype = np.float32) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+    return rescaled_image
+
+def resize(image: Image, size: Tuple[int, int], resample: Image.Resampling = None, reducing_gap: Optional[int] = None) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize((width, height), resample=resample, reducing_gap=reducing_gap)
+    return resized_image
+
+def normalize(image: np.ndarray, mean: Union[float, Iterable[float]], std: Union[float, Iterable[float]]) -> np.ndarray:
+    mean = np.array(mean, dtype=image.dtype)
+    std = np.array(std, dtype=image.dtype)
+    image = (image - mean) / std
+    return image
+
 
 def process_images(images: List[Image.Image], size: Dict[str, int] = None, resample: Image.Resampling = None, rescale_factor: float = None, 
                     image_mean: Optional[Union[float, List[float]]] = None, image_std: Optional[Union[float, List[float]]] = None) -> List[np.ndarray]:
